7444d01fb6efbec7f9707e1179e563f023ea1df9
Magnus <magnus@bonusfamilien.dk>
2024-04-03 12:31:44 +0200

    Server Setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # FestFriendServer
 ID Server for festfriend to create and join groups
+
+# Setup
+```sh
+python -m venv .venv
+.venv/Scripts/Activate.ps1
+python -m pip install -r requirements.txt
+```
+
+Activate script to use depends on the operating system and shell
+
+# Run Server in development
+```sh
+uvicorn server:app --reload
+```
---
+++ server.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, HTTPException, status
+import bcrypt
+import random
+import string
+
+app = FastAPI()
+
+groups: dict[str, list[str]] = dict()
+groupLogin: dict[str, str] = dict()
+
+def getID():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+def generate_group_id(groupIDs: list[str]):
+    groupID = getID()
+    while groupID in groupIDs:
+        groupID = getID()
+    return groupID
+    
+def hash_password(password: str):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds=12))
+    
+def check_password(password: str, grouphash: str):
+    return bcrypt.checkpw(password.encode(), grouphash)
+
+def is_duplicate_user(username: str, usernames: list[str]):
+    return username in usernames
+    
+@app.post("/join")
+async def join_group(groupID: str, username: str, password:str):
+    if groupID not in groupLogin:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Group {groupID} not found!")
+    
+    if not check_password(password + groupID, groupLogin[groupID]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong groupID or password!")
+    
+    if is_duplicate_user(username, groups[groupID]):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"{username} already exists!")
+    
+    groups[groupID].append(username)
+    
+    return {"message": "OK"}
+    
+@app.post("/group/create")
+async def create_group(password: str):
+    groupID = generate_group_id(groupLogin.values())
+    hashed = hash_password(password + groupID)
+    groupLogin[groupID] = hashed
+    groups[groupID] = []
+    return {"groupID": groupID}
+
+@app.post("/group/members")
+async def get_members(groupID: str, password: str):
+    if groupID not in groupLogin:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Group {groupID} not found!")
+    
+    if not check_password(password + groupID, groupLogin[groupID]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong groupID or password!")
+    
+    return {"members": groups[groupID]}
